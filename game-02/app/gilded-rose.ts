a16d29d21f6223d8118a08ffe6650d104e20bc30
Cristián Quintana <cristianmanuelquintana@gmail.com>
2022-02-15 11:33:22 -0300

    no case on keeping those if

--- game-02/app/gilded-rose.ts
+++ game-02/app/gilded-rose.ts
@@ -22,42 +22,40 @@ export class GildedRose {
             if(this.items[i].name != 'Sulfuras, Hand of Ragnaros'){
                 this.items[i].sellIn = this.items[i].sellIn - 1; 
             }
-            
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name == 'Conjured'){
-                        if(this.items[i].sellIn >= 0 ){
-                            this.items[i].quality = this.items[i].quality - 2
-                        }else{
-                            this.items[i].quality = this.items[i].quality >= 4 ? this.items[i].quality - 4 : 0
-                        }
+            switch (this.items[i].name ){
+                case 'Aged Brie':
+                    this.items[i].quality = this.items[i].quality < 50 ? this.items[i].quality + 1 : this.items[i].quality 
+                    break;
+                case 'Backstage passes to a TAFKAL80ETC concert':
+                    if (this.items[i].sellIn <= 0){
+                        this.items[i].quality = 0
+                    }else if (this.items[i].sellIn <= 5){
+                        this.items[i].quality = this.items[i].quality < 48 ? this.items[i].quality + 3 : 50
+                    }else if(this.items[i].sellIn <= 10){
+                        this.items[i].quality = this.items[i].quality < 49 ? this.items[i].quality + 2 : 50
                     }else{
-                        if(this.items[i].sellIn >= 0 ){
-                            this.items[i].quality = this.items[i].quality - 1
-                        }else{
-                            this.items[i].quality = this.items[i].quality >= 2 ? this.items[i].quality - 2 : 0
-                        }
-                    }           
-                }
-            } else {
-                switch (this.items[i].name ){
-                    case 'Aged Brie':
-                        this.items[i].quality = this.items[i].quality < 50 ? this.items[i].quality + 1 : this.items[i].quality 
-                        break;
-                    case 'Backstage passes to a TAFKAL80ETC concert':
-                        if (this.items[i].quality < 50 ){
-                            if (this.items[i].sellIn <= 0){
-                                this.items[i].quality = 0
-                            }else if (this.items[i].sellIn <= 5){
-                                this.items[i].quality = this.items[i].quality < 48 ? this.items[i].quality + 3 : 50
-                            }else if(this.items[i].sellIn <= 10){
-                                this.items[i].quality = this.items[i].quality < 49 ? this.items[i].quality + 2 : 50
+                        this.items[i].quality = this.items[i].quality < 50 ? this.items[i].quality + 1 : this.items[i].quality
+                    }
+                    break;
+                case 'Sulfuras, Hand of Ragnaros':
+                    break;
+                default:
+                    if (this.items[i].quality > 0) {
+                        if (this.items[i].name == 'Conjured'){
+                            if(this.items[i].sellIn >= 0 ){
+                                this.items[i].quality = this.items[i].quality - 2
                             }else{
-                                this.items[i].quality = this.items[i].quality + 1 
+                                this.items[i].quality = this.items[i].quality >= 4 ? this.items[i].quality - 4 : 0
                             }
-                        }
-                        break;
-                }
+                        }else{
+                            if(this.items[i].sellIn >= 0 ){
+                                this.items[i].quality = this.items[i].quality - 1
+                            }else{
+                                this.items[i].quality = this.items[i].quality >= 2 ? this.items[i].quality - 2 : 0
+                            }
+                        }           
+                    }
+                    break;
             }
         }
 
