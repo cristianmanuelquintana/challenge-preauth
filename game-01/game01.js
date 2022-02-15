7f68026e948a10364395d5a3828f07fb8d3eb31b
Cristián Quintana <cristianmanuelquintana@gmail.com>
2022-02-15 14:02:12 -0300

    improved game01

--- game-01/game01.js
+++ game-01/game01.js
@@ -1,14 +1,18 @@
 function getSummandSubList(nList, mSum){
+    let subSet = new Array(nList.length)
     while(nList.length > 1){
         const first = nList.shift();
-        const numSearched = mSum - first
-        if(nList.includes(numSearched)){
-            return [first, numSearched];
+        if(first < mSum){
+            const numSearched = mSum - first
+            if(subSet[numSearched]==1){
+                return [first, numSearched]
+            }
+            subSet[first] = 1
         }
     }return [];
 }
 (function(){
-    const nList = [2,3,4,5,6,17,8,9]
+    const nList = [2,3,4,5,17,9]
     const mSum = 10
     const result = getSummandSubList(nList, mSum);
     if(result.length > 0){
